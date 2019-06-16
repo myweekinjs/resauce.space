f0b47b8f97e27920e526c2e561b9829f4b0c36a2
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-06-16 20:16:26 +1000

    now config update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
+const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
 
-module.exports = withTypescript(withCSS({}))
+// module.exports = withTypescript(withCSS({}))
+
+module.exports = withPlugins([
+  [withTypescript],
+  [withCSS]
+], {
+  target: "serverless"
+})
